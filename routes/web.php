dfd1920befe46962dd0dbae20ce68f35055a1fcb
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-01-30 14:12:55 +0700

    Konversi Category Page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,6 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
 
 Auth::routes();
