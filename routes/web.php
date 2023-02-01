c7e7b974bb4af9b1c773c2672b79b3d33a2db9c6
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-02 06:13:42 +0700

    Konversi Dashboard Transaction Page

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\CartController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DashboardProductController;
+use App\Http\Controllers\DashboardTransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,10 +26,15 @@ Route::get('/categories', [CategoryController::class, 'index'])->name('categorie
 Route::get('/details/{id}', [DetailController::class, 'index'])->name('detail');
 Route::get('/cart', [CartController::class, 'index'])->name('cart');
 Route::get('/success', [CartController::class, 'success'])->name('success');
+
 Route::get('/register/success', [RegisterController::class, 'success'])->name('register-success');
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
 Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-products');
 Route::get('/dashboard/products/create', [DashboardProductController::class, 'create'])->name('dashboard-products-create');
 Route::get('/dashboard/products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-products-details');
+
+Route::get('/dashboard/transactions', [DashboardTransactionController::class, 'index'])->name('dashboard-transactions');
 
 Auth::routes();
