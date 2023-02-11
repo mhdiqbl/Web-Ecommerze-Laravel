d37c3f2ddaf1c94e9ee076ea98385e7b7da5437a
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-11 13:27:06 +0700

    Add Model Product and Product Gallery

---
+++ app/Models/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'users_id', 'categories_id', 'price', 'description', 'slug'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function gallery()
+    {
+        return $this->hasMany(ProductGallery::class, 'products_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'users_id', 'id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'categories_id', 'id');
+    }
+}
