753ac0994f3adad5d1af98afe19bb0b21aed2133
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-12 13:40:45 +0700

    page for view product

--- resources/views/pages/admin/product/index.blade.php
+++ resources/views/pages/admin/product/index.blade.php
@@ -5,31 +5,37 @@
 @endsection
 
 @section('content')
-    <div class="section-content section-dashboard-home" data-aos="fade-up">
+    <!-- Section Content -->
+    <div
+        class="section-content section-dashboard-home"
+        data-aos="fade-up"
+    >
         <div class="container-fluid">
             <div class="dashboard-heading">
                 <h2 class="dashboard-title">Product</h2>
-                <p class="dashboard-subtitle">List of Products</p>
+                <p class="dashboard-subtitle">
+                    List of Product
+                </p>
             </div>
             <div class="dashboard-content">
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-body">
-                                <a href="{{route('product.create')}}" class="btn btn-primary mb-3">
-                                    + Add New Product
+                                <a href="{{  route('product.create') }}" class="btn btn-primary mb-3">
+                                    + Tambah Product Baru
                                 </a>
                                 <div class="table-responsive">
                                     <table class="table table-hover scroll-horizontal-vertical w-100" id="crudTable">
                                         <thead>
-                                            <tr>
-                                                <th>ID</th>
-                                                <th>Name</th>
-                                                <th>Pemilik</th>
-                                                <th>Category</th>
-                                                <th>Harga</th>
-                                                <th>Aksi</th>
-                                            </tr>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nama</th>
+                                            <th>Pemilik</th>
+                                            <th>Kategori</th>
+                                            <th>Harga</th>
+                                            <th>Aksi</th>
+                                        </tr>
                                         </thead>
                                         <tbody></tbody>
                                     </table>
@@ -43,9 +49,10 @@
     </div>
 @endsection
 
+
 @push('addon-script')
     <script>
-        // AJAX DataTable
+        // AJAX DataTablenn
         var datatable = $('#crudTable').DataTable({
             processing: true,
             serverSide: true,
@@ -57,7 +64,7 @@
                 { data: 'id', name: 'id' },
                 { data: 'name', name: 'name' },
                 { data: 'user.name', name: 'user.name' },
-                { data: 'category.name', name: 'username' },
+                { data: 'category.name', name: 'category.name' },
                 { data: 'price', name: 'price' },
                 {
                     data: 'action',
