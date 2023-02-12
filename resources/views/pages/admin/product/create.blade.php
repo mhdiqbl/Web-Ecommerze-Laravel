d9fbd7ada338538bb3993cd84d0184ffd1261be9
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-12 13:40:37 +0700

    page for create product

--- resources/views/pages/admin/product/create.blade.php
+++ resources/views/pages/admin/product/create.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.admin')
 
 @section('title')
-    User
+    Product
 @endsection
 
 @section('content')
     <div class="section-content section-dashboard-home" data-aos="fade-up">
         <div class="container-fluid">
             <div class="dashboard-heading">
-                <h2 class="dashboard-title">User</h2>
-                <p class="dashboard-subtitle">Create New User</p>
+                <h2 class="dashboard-title">Product</h2>
+                <p class="dashboard-subtitle">Create New Product</p>
             </div>
             <div class="dashboard-content">
                 <div class="row">
@@ -25,34 +25,46 @@
                         @endif
                         <div class="card">
                             <div class="card-body">
-                                <form action="{{ route('user.store') }}" method="POST" enctype="multipart/form-data">
+                                <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data">
                                     @csrf
                                     <div class="row">
                                         <div class="col-md-12">
                                             <div class="form-group">
-                                                <label>Name User</label>
+                                                <label>Name Product</label>
                                                 <input type="text" name="name" value="{{ old('name') }}" class="form-control" required>
                                             </div>
                                         </div>
                                         <div class="col-md-12">
                                             <div class="form-group">
-                                                <label>Email User</label>
-                                                <input type="email" name="email" class="form-control" value="{{ old('email') }}" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>Password User</label>
-                                                <input type="password" name="password" class="form-control" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>Roles</label>
-                                                <select name="roles" class="form-control" required>
-                                                    <option value="ADMIN">Admin</option>
-                                                    <option value="User">User</option>
+                                                <label>Pemilik Product</label>
+                                                <select name="users_id" class="form-control">
+                                                    @foreach($users as $user)
+                                                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                                    @endforeach
                                                 </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Kategori Product</label>
+                                                <select name="categories_id" class="form-control">
+                                                    @foreach($categories as $category)
+                                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Harga Product</label>
+                                                <input type="number" name="price" value="{{ old('price') }}" class="form-control" required>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Deskripsi Product</label>
+                                                <textarea name="description" id="editor">
+                                                </textarea>
                                             </div>
                                         </div>
                                     </div>
@@ -72,3 +84,10 @@
         </div>
     </div>
 @endsection
+
+@push('addon-script')
+    <script src="https://cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'editor' );
+    </script>
+@endpush
