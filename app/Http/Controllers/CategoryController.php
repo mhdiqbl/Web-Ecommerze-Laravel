37660c90a8175f19d4d1715f39c5d2b6ee67d9bb
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-13 21:39:17 +0700

    throw data category to page categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return view('pages.category');
+        $categories = Category::all();
+        $products = Product::with('galleries')->paginate(1);
+        return view('pages.category', [
+            'categories' => $categories,
+            'products' => $products,
+        ]);
     }
 }
