0ef79f36f39e18de96ef23702ba80158156cbe0b
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-13 22:23:42 +0700

    Update Navigation Section

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -37,7 +37,8 @@
                             <a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a>
                             <a class="dropdown-item" href="{{ route('dashboard-settings-account') }}">Setting</a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
+                            <a class="dropdown-item"
+                                href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">Logout</a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
