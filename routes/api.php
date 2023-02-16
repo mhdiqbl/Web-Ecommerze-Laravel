280f8bf144b57afd572083ac99757946ec1e70f2
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-16 17:55:00 +0700

    Membuat API Location dari Indoregion

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\API\LocationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/register/check', [RegisterController::class, 'check'])->name('api-register-check');
+Route::get('register/check', [RegisterController::class, 'check'])->name('api-register-check');
+Route::get('provinces', [LocationController::class, 'provinces'])->name('api-provinces');
+Route::get('regencies/{provinces_id}', [LocationController::class, 'regencies'])->name('api-regencies');
