18259bc549a190a01b400757950016de28e66190
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-19 17:51:35 +0700

    Mengubah Model Transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,10 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Transaction extends Model
 {
     use HasFactory, SoftDeletes;
 
+    protected $fillable = [
+        'users_id',
+        'insurance_price',
+        'shipping_price',
+        'total_price',
+        'code',
+    ];
 
+    protected $hidden = [
+
+    ];
 }
