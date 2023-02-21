3ebf7ef860cb74b587c8317626063267e6c9a4cd
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-22 01:51:37 +0700

    Add Action for Product

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -79,7 +79,7 @@ class AdminProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
         $data = $request->all();
 
