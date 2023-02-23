246651b19cd85164baa61482b2bef3b04b176e39
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2023-02-23 12:17:54 +0700

    Midtrans Callback

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Midtrans\Config;
+use Midtrans\Notification;
 use Midtrans\Snap;
 
 class CheckoutController extends Controller
@@ -90,6 +91,55 @@ class CheckoutController extends Controller
 
     public function callback(Request $request)
     {
-        //
+        //Set konfigurasi midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+
+        //Instance midtrans notification
+        $notification = new Notification();
+
+        //Assign variable untuk memudahkan coding
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $fraud = $notification->fraud_status;
+        $order_id = $notification->order_id;
+
+
+        //Cari transaksi berdasarkan berdasarkan 1D
+        $transaction = Transaction::findOrFail($order_id);
+
+        //Handle notification status
+        if ($status == 'capture'){
+            if ($type == 'credit_card'){
+                if ($fraud == 'challenge'){
+                    $transaction->status = 'PENDING';
+                }
+                else {
+                    $transaction->status = 'SUCCESS';
+                }
+            }
+        }
+
+        elseif ($status == 'settlement'){
+            $transaction->status = 'SUCCESS';
+        }
+        elseif ($status == 'pending'){
+            $transaction->status = 'PENDING';
+        }
+        elseif ($status == 'deny'){
+            $transaction->status = 'CANCELLED';
+        }
+        elseif ($status == 'expire'){
+            $transaction->status = 'CANCELLED';
+        }
+        elseif ($status == 'cancel'){
+            $transaction->status = 'CANCELLED';
+        }
+
+        //Simpan transaksi
+        $transaction->save();
     }
 }
